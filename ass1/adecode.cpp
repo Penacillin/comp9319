ab6ccfd98324177d05881fa6e66b585728bc763a
Nevin <nevinsuri@hotmail.com>
2020-06-19 18:32:43 +1000

    fix for BILL GATES with epsilon mpfr

--- ass1/adecode.cpp
+++ ass1/adecode.cpp
@@ -9,13 +9,52 @@
 
 #define MAX_LENGTH 2048
 
+mpfr_t MPFR_EPSILON;
+
+// A < B - E
+// A - B < -E
+// B - A > E
+bool my_mpfr_less(const mpfr_t val_1, const mpfr_t val_2) {
+    mpfr_t temp;
+    mpfr_init2(temp, 256);
+    mpfr_sub(temp, val_2, val_1, rnd);
+    // mpfr_fprintf(stderr, "adecode: my_mpfr_less: %.75Rf-%.75Rf=%.75Rf>%.75Rf\n", val_2, val_1, temp, MPFR_EPSILON);
+    if (mpfr_greater_p(temp, MPFR_EPSILON)) {
+        mpfr_clear(temp);
+        return true;
+    }
+    mpfr_clear(temp);
+    return false;
+}
+
+// A >= B - E
+// A - B >= -E
+// B - A <= E
+bool my_mpfr_greaterequal_p(const mpfr_t val_1, const mpfr_t val_2) {
+    mpfr_t temp;
+    mpfr_init2(temp, 256);
+    mpfr_sub(temp, val_2, val_1, rnd);
+    // mpfr_fprintf(stderr, "adecode: my_mpfr_greaterequal_p: %.75Rf-%.75Rf=%.75Rf>%.75Rf\n",
+    //              val_1, val_2, temp, MPFR_EPSILON);
+    if (mpfr_lessequal_p(temp, MPFR_EPSILON)) {
+        mpfr_clear(temp);
+        return true;
+    }
+    mpfr_clear(temp);
+    return false;
+}
+
 template<size_t Size>
 char get_symbol_for_code(const mpfr_t ac_val,
+                         const int (&count_table)[Size],
                          const mpfr_t (&low_table)[Size],
                          const mpfr_t (&high_table)[Size]) {
+    // mpfr_fprintf(stderr, "get_symbol_for_code: %.15Rf\n", ac_val);
     for (size_t i = 0; i < Size; ++i) {
-        if (mpfr_greaterequal_p(ac_val, low_table[i]) && mpfr_less_p(ac_val, high_table[i])) {
-            mpfr_fprintf(stderr, "%.9Rf <= %.70Rf < %.70Rf\n", low_table[i], ac_val, high_table[i]);
+        if (count_table[i] > 0 &&
+                my_mpfr_greaterequal_p(ac_val, low_table[i]) &&
+                my_mpfr_less(ac_val, high_table[i])) {
+            // mpfr_fprintf(stderr, "%.9Rf <= %.70Rf < %.70Rf\n", low_table[i], ac_val, high_table[i]);
             return (char) i;
         }
     }
@@ -26,6 +65,8 @@ char get_symbol_for_code(const mpfr_t ac_val,
 
 int main(void)
 {
+    mpfr_init2(MPFR_EPSILON, 256);
+    mpfr_set_str(MPFR_EPSILON, "0.000000000000000000000000000000000000000000000000000000000000005", 10, rnd);
     std::locale loc;
 
     int count_table[256] = {0};
@@ -34,13 +75,11 @@ int main(void)
     mpfr_t high_table[256];
     mpfr_t ac_val;
     mpfr_init2(ac_val, 256);
-    printf("adecode: %ld\n", mpfr_get_emin());
     char buffer[MAX_LENGTH];
     // count each char in input
     while (fgets(buffer, MAX_LENGTH, stdin) != NULL)
     {
         if (buffer[1] == '.') {
-            fprintf(stderr, "adecode: ac_val buffer: %s\n", buffer);
             mpfr_strtofr(ac_val, buffer, nullptr, 10, rnd);
             break;
         }
@@ -63,9 +102,8 @@ int main(void)
     mpfr_t code_range;
     mpfr_init2(code_range, 256);
     for (size_t i = 0; i < char_count; ++i) {
-        char symbol = get_symbol_for_code(ac_val, low_table, high_table);
+        char symbol = get_symbol_for_code(ac_val, count_table, low_table, high_table);
         putchar(symbol);
-        mpfr_fprintf(stderr, "adecode: ac_val: %.9Rf (%c)\n", ac_val, symbol);
         mpfr_sub(code_range, high_table[(size_t)symbol], low_table[(size_t)symbol], rnd);
         mpfr_sub(ac_val, ac_val, low_table[(size_t)symbol], rnd);
         mpfr_div(ac_val, ac_val, code_range, rnd);
