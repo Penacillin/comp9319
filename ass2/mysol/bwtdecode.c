6e5bbc4e5d627a89027bb5a0c7cbdc4212a7387f
Nevin <nevinsuri@hotmail.com>
2020-07-07 17:17:29 +1000

    begin moving rank table

--- ass2/mysol/bwtdecode.c
+++ ass2/mysol/bwtdecode.c
@@ -9,40 +9,52 @@
 #include <assert.h>
 
 #define ENDING_CHAR '\n'
+#define TABLE_SIZE 4096
+#define PAGE_TABLE_SIZE 100000000/TABLE_SIZE
+
 
 typedef struct _rank_entry {
     char symbol;
     unsigned matching;
 } RankEntry;
 
+
 typedef struct _BWTDecode {
-    RankEntry rankTable[1024];
-    unsigned rankTableStart;
-    unsigned rankTableEnd;
-    unsigned rankTableSize;
-    char in_buffer[1024];
+    u_int32_t runCount[128];
+    u_int32_t runCountCum[PAGE_TABLE_SIZE][4];
+    RankEntry rankTable[TABLE_SIZE];
+    u_int32_t rankTableStart;
+    u_int32_t rankTableEnd;
+    u_int32_t rankTableSize;
 
-    unsigned endingCharRankIndex;
+    char in_buffer[TABLE_SIZE];
 
-    unsigned CTable[128];
-    unsigned runCount[128];
+    u_int32_t CTable[128];
 
     int bwt_file_fd;
 } BWTDecode;
 
-const char LANGUAGE[5] = {'\n', 'A', 'C', 'G', 'T'};
+const size_t BWTDECODE_SIZE = sizeof(struct _BWTDecode);
+
+const unsigned LANGUAGE[5] = {'\n', 'A', 'C', 'G', 'T'};
 
 static inline unsigned get_char_index(const char c) {
     switch(c)  {
-        case '\n': return 0;
         case 'A': return 1;
         case 'C': return 2;
         case 'G': return 3;
         case 'T': return 4;
+        case '\n': return 0;
     };
-    fprintf(stderr, "nani tf >%d<\n", c);
+#ifdef DEBUG
+    fprintf(stderr, "FATAL UNKOWN CHARACTER %d\n", c);
     exit(1);
+#endif
 }
+
+// A C G T
+// 0 2 6 9
+// 0 1 2 3
 
 off_t read_file(BWTDecode* decode_info, char *bwt_file_path) {
     int fd = open(bwt_file_path, O_RDONLY);
@@ -67,14 +79,16 @@ off_t read_file(BWTDecode* decode_info, char *bwt_file_path) {
 void build_tables(BWTDecode *decode_info) {
     ssize_t k;
     unsigned curr_index = 0;
-    while((k = read(decode_info->bwt_file_fd, decode_info->in_buffer, 1024)) > 0) {
+    unsigned page_index = 0;
+    while((k = read(decode_info->bwt_file_fd, decode_info->in_buffer, TABLE_SIZE)) > 0) {
         decode_info->rankTableStart = curr_index;
+        assert(page_index < PAGE_TABLE_SIZE);
+        for (unsigned i = 0; i < 4; ++i)
+            decode_info->runCountCum[page_index][i] = decode_info->runCount[LANGUAGE[i]];
+        ++page_index;
         for (ssize_t i = 0; i < k; ++i) {
             // Add to all CTable above char
             const char c = decode_info->in_buffer[i];
-            if (c == 0) {
-                fprintf(stderr, "unlucky %ld %ld %d\n", k, i, curr_index);
-            }
             for (unsigned j = get_char_index(c) + 1; j < 5; ++j) {
                 ++(decode_info->CTable[(unsigned)LANGUAGE[j]]);
             }
@@ -100,6 +114,28 @@ void print_ranktable(const BWTDecode *decode_info) {
 }
 
 void ensure_in_rank_table(BWTDecode *decode_info, const unsigned index) {
+    if (decode_info->rankTableStart <= index && decode_info->rankTableEnd >= index) return;
+
+    const unsigned desired_page_index = index / TABLE_SIZE;
+#ifdef DEBUG
+    fprintf(stderr, "Moving page to %d\n", desired_page_index);
+#endif
+    decode_info->rankTableStart =  desired_page_index * TABLE_SIZE;
+    lseek(decode_info->bwt_file_fd, decode_info->rankTableStart, SEEK_SET);
+    const ssize_t read_bytes = read(decode_info->bwt_file_fd, decode_info->in_buffer, TABLE_SIZE);
+    decode_info->rankTableEnd = decode_info->rankTableStart + read_bytes;
+    decode_info->rankTableSize = read_bytes;
+
+    unsigned tempRunCount[128] = {0};
+    // Load in char counts until this page
+    for (unsigned i = 0; i < 4; ++i)
+        tempRunCount[LANGUAGE[i]] = decode_info->runCountCum[desired_page_index][i];
+    // Fill out char counts for this page
+    for (unsigned i = 0; i < read_bytes; ++i) {
+        const char c = decode_info->in_buffer[i];
+        decode_info->rankTable[i] = (RankEntry){c,  tempRunCount[(unsigned)c]++};
+    }
+
     assert((decode_info->rankTableStart <= index && decode_info->rankTableEnd >= index));
     return;
 }
@@ -108,7 +144,7 @@ int do_stuff2(BWTDecode *decode_info,
               off_t file_size,
               char* output_file_path,
               unsigned int a2) {
-    int out_fd = open(output_file_path, O_CREAT | O_WRONLY);
+    int out_fd = open(output_file_path, O_CREAT | O_WRONLY, 0666);
     if (out_fd < 0) {
         char *err = strerror(errno);
         printf("Could not create the reversed file %s - %s\n", output_file_path, err);
@@ -124,29 +160,38 @@ int do_stuff2(BWTDecode *decode_info,
     unsigned index = decode_info->CTable[ENDING_CHAR];
     printf("starting index %d\n", index);
 
-    char output_buffer[1024];
-    unsigned output_buffer_index = sizeof(output_buffer) - 1;
-    output_buffer[output_buffer_index--] = ENDING_CHAR;
+    char output_buffer[TABLE_SIZE];
+    unsigned output_buffer_index = sizeof(output_buffer);
+    output_buffer[--output_buffer_index] = ENDING_CHAR;
     --file_size;
 
     while (file_size > 0) {
         ensure_in_rank_table(decode_info, index);
-        const char out_char = decode_info->rankTable[index].symbol;
-        output_buffer[output_buffer_index--] = out_char;
+        const char out_char = decode_info->rankTable[index % TABLE_SIZE].symbol;
+        output_buffer[--output_buffer_index] = out_char;
+        --file_size;
 
         if (output_buffer_index == 0) {
             lseek(out_fd, file_size, SEEK_SET);
-            write(out_fd, output_buffer, 1024);
-            output_buffer_index = sizeof(output_buffer) - 1;
+            printf("File Size %ld, index %d\n", file_size, index);
+            ssize_t res = write(out_fd, output_buffer, sizeof(output_buffer));
+            if (res != sizeof(output_buffer)) exit(1);
+            output_buffer_index = sizeof(output_buffer);
         }
 
-        index = decode_info->rankTable[index].matching + decode_info->CTable[(unsigned)out_char];
-
-        --file_size;
+        index = decode_info->rankTable[index % TABLE_SIZE].matching + decode_info->CTable[(unsigned)out_char];
+        fprintf(stderr, "index: %d\n", index);
     }
 
     lseek(out_fd, file_size, SEEK_SET);
-    write(out_fd, output_buffer + output_buffer_index, sizeof(output_buffer) - output_buffer_index);
+    ssize_t res = write(
+        out_fd,
+        output_buffer + output_buffer_index,
+        sizeof(output_buffer) - output_buffer_index);
+    if ((unsigned)res != sizeof(output_buffer) - output_buffer_index) {
+        fprintf(stderr, "Error writing to file. Wrote %ld instead of %d\n", res, TABLE_SIZE);
+        exit(1);  
+    }
 
     close(out_fd);
 
